b271771ef679316020ff83b4984940b94ada23b7
Kaasiak <kasia.kobalczyk@gmail.com>
2022-01-20 20:00:44 +0000

    Trasnform part 1 to slides

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import norm
 import pandas as pd
+import random
 
 def revert_diff(diff_values, start_values, period):
     x = np.r_[start_values, diff_values]
@@ -10,9 +11,10 @@ def revert_diff(diff_values, start_values, period):
     return x
 
 # Example Time Series - weekly sales
+random.seed(11)
 y = np.zeros(52)
 for i in range(1, 52):
-    y[i] = y[i - 1] + norm.rvs(size=1, scale=5)
+    y[i] = y[i - 1] + norm.rvs(size=1, scale=2)
 x = np.arange(1, 53, 1)
 sales_df = pd.DataFrame(np.stack([x, y], axis=1), columns=["week", "volume"])
 sales_df.to_csv("data/weekly-sales.csv", index=False)
